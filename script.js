771ecc3367e12553b1ecf7aad8f634a486f6eb2e
dhruvidave09 <dhruvindave86@gmail.com>
2026-07-08 17:52:55 +0000

    push

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello! The website loaded successfully.");
+
+const omgButton = document.getElementById("omgBox");
+
+omgButton.addEventListener("click", function() {
+  console.log("WAITTT DAMNNNN TS SO COOLLL SONN");
+});
